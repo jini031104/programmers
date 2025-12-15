4518aefdda28d874a6c426f081df95227bec82e4
jini031104 <jini031104@naver.com>
2025-12-15 18:25:23 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=1; i*i<=n; i++){
+        if(i*i == n)
+            answer++;
+        else if(n % i == 0)
+            answer+=2;
+    }
+    
+    return answer;
+}
