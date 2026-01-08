dd063b0967ea0c8f7b7487f54e0c34cb28a5b4d1
jini031104 <jini031104@naver.com>
2026-01-08 10:27:03 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cpp
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cpp
@@ -1,24 +1,17 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> numbers, string direction) {
-    vector<int> answer;
-    int num = 0;
-    
-    if(direction == "right"){
-        answer.push_back(numbers[numbers.size() - 1]);
-        for(int i=0; i<numbers.size() - 1; i++){
-            answer.push_back(numbers[i]);
-        }
+    if(direction =="left")
+    {
+        rotate(numbers.begin(),numbers.begin()+1,numbers.end());
     }
-    else{
-        for(int i=0; i<numbers.size() - 1; i++){
-            answer.push_back(numbers[i + 1]);
-        }
-        answer.push_back(numbers[0]);
+    else
+    {
+        rotate(numbers.begin(),numbers.end()-1,numbers.end());
     }
-    
-    return answer;
+
+    return numbers;
 }
