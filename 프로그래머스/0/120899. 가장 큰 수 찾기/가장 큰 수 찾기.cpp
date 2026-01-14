ae23884e029b77ffb6af1d08ed91e8d5e386d5b2
jini031104 <jini031104@naver.com>
2026-01-15 04:49:03 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array) {
+    vector<int> answer;
+    int max = array[0], max_index = 0;
+    
+    for(int i=1; i<array.size(); i++){
+        if(max < array[i]){
+            max = array[i];
+            max_index = i;
+        }
+    }
+    
+    answer.push_back(max);
+    answer.push_back(max_index);
+    
+    return answer;
+}
