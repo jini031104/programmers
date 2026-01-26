c0ed89601a2e4e1358b7d617487783b9325eaaf7
jini031104 <jini031104@naver.com>
2026-01-26 21:01:51 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -5,17 +5,6 @@ using namespace std;
 
 int solution(string my_string, string is_prefix) {
     int answer = 0;
-    string c = "";
     
-    if(my_string.size() < is_prefix.size())
-        return 0;
-
-    for(int i=0; i<is_prefix.size(); i++){
-        c += my_string[i];
-    }
-    
-    if(c == is_prefix)
-        answer = 1;
-    
-    return answer;
+    return !my_string.find(is_prefix);
 }
