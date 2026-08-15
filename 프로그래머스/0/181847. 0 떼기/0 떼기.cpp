0d2f84f59458ab40564c7635ab42a3daade56d0c
jini031104 <jini031104@naver.com>
2026-08-15 09:06:39 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -4,16 +4,7 @@
 using namespace std;
 
 string solution(string n_str) {
-    string answer = "";
-    bool test = false;
-    
-    for(int i=0; i<n_str.size(); i++){
-        if(!test && n_str[i] == '0')
-            continue;
-        
-        answer += n_str[i];
-        test = true;
-    }
+    string answer = to_string(stoi(n_str));
     
     return answer;
 }
