e0656d33e7f192de8e0ecc7d45364696f2867791
jini031104 <jini031104@naver.com>
2026-08-31 15:33:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <bitset>
 
 using namespace std;
 
@@ -10,59 +9,16 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     
     for(int i=0; i<n; i++){
         string s;
-        while(arr1[i] != 0){
-            if (arr1[i] % 2 == 1) // 나머지가 1
-                s += '1';
-            else			// 나머지가 0
-                s += '0';
-            
-            arr1[i] /= 2;
-        }
-        
-        while(s.size() != n)
-            s += '0';
-        
-        reverse(s.begin(), s.end());
-        v1.push_back(s);
-    }
-    for(int i=0; i<n; i++){
-        string s;
-        while(arr2[i] != 0){
-            if (arr2[i] % 2 == 1) // 나머지가 1
-                s += '1';
-            else			// 나머지가 0
-                s += '0';
-            
-            arr2[i] /= 2;
-        }
-        
-        while(s.size() != n)
-            s += '0';
-        
-        reverse(s.begin(), s.end());
-        v2.push_back(s);
-    }
-    
-    for(int i=0; i<n; i++){
-        string s;
+        arr1[i] = arr1[i] | arr2[i];
         for(int j=0; j<n; j++){
-            if(v1[i][j] == '1' || v2[i][j] == '1')
-                s += '1';
-            else
-                s += '0';
-        }
-        
-        v3.push_back(s);
-    }
-    
-    for(int i=0; i<n; i++){
-        string s;
-        for(int j=0; j<n; j++){
-            if(v3[i][j] == '1')
-                s += '#';
-            else
+            if(arr1[i] % 2 == 0)
                 s += ' ';
+            else
+                s += '#';
+            
+            arr1[i] = arr1[i] >> 1;
         }
+        reverse(s.begin(), s.end());
         answer.push_back(s);
     }
     
