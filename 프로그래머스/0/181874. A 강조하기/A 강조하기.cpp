8111c14dd36e23b8217aedbb87e69897e25289d7
jini031104 <jini031104@naver.com>
2026-09-07 13:27:04 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    for(int i=0; i<myString.size(); i++){
+        if(myString[i] == 'a' || myString[i] == 'A')
+            answer += 'A';
+        else
+            answer += tolower(myString[i]);
+    }
+    
+    return answer;
+}
